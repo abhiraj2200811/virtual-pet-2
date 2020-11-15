72fc284b6ec7c1729453e3d911c28bdb47d47fd3
abhiraj2200811 <70210909+abhiraj2200811@users.noreply.github.com>
2020-11-15 16:58:07 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,26 @@
+class Milk  {
+
+    constructor(x,y) {
+    
+var foodStock;
+var lastFed; 
+
+this.image=loadImage("images/Milk.png")
+    
+    }
+
+    display(){
+       
+    var x=80,y=100;
+
+    imageMode(CENTER);
+    image(this.image,720,220,70,70);
+
+    
+
+    }
+
+}
+
+
+
